c2c725c8c96fb92a91cbdec1f9f0b76dd53820aa
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-02-23 17:36:03 +0530

    added js code for changing color of donation icon and event comment

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function colorred() {
+	var ch = document.getElementById("donate-button").childNodes;
+	ch[1].style.color = "red";
+}
+
+function colorwhite() {
+	var ch = document.getElementById("donate-button").childNodes;
+	ch[1].style.color = "white";
+}
+
+
+function displaycomment1() {	
+    var x = document.getElementById("comment1");
+    x.style.display = "block";
+}
+
+function hidecomment1() {
+	var x = document.getElementById("comment1");
+    x.style.display = "none";
+}
+
+function displaycomment2() {	
+    var x = document.getElementById("comment2");
+    x.style.display = "block";
+}
+
+function hidecomment2() {
+	var x = document.getElementById("comment2");
+    x.style.display = "none";
+}
+
+function displaycomment3() {	
+    var x = document.getElementById("comment3");
+    x.style.display = "block";
+}
+
+function hidecomment3() {
+	var x = document.getElementById("comment3");
+    x.style.display = "none";
+}
+
+function displaycomment4() {	
+    var x = document.getElementById("comment4");
+    x.style.display = "block";
+}
+
+function hidecomment4() {
+	var x = document.getElementById("comment4");
+    x.style.display = "none";
+}
